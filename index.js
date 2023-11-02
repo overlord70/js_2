b15b7689de7a0f185baa7888978fad6e276b660f
overlord70 <s37776270@gmail.com>
2023-11-02 15:26:56 +0500

    done new homework

--- index.js
+++ index.js
@@ -1,73 +1,293 @@
-let arr = [
+let cars = [
     {
-        name: 'Sardor',
-        age: 15,
-        ismarried: false
+        model: "malibu",
+        company: "general motors",
+        color: "dry asphalt",
+        engine: 2.4,
+        price: 30000,
+        beaten: false,
+        year: 2022,
+        mileage: 1000,
+        tech_passport: {
+            date: 2022,
+            exp_date: 2032,
+        }
     },
     {
-        name: 'Ismail',
-        age: 12,
-        ismarried:false
+        model: "tracker",
+        company: "general motors",
+        color: "black",
+        engine: "2.2",
+        price: 22000,
+        beaten: false,
+        year: 2021,
+        mileage: 2000,
+        tech_passport: {
+            date: 2021,
+            exp_date: 3031
+        }
     },
     {
-        name: 'Allayor',
-        age: 14,
-        ismarried: false
+        model: "gentra",
+        company: "general motors",
+        color: "black",
+        engine: 1.5,
+        price: 17000,
+        beaten: false,
+        year: 2021,
+        mileage: 6000,
+        tech_passport: {
+            date: 2021,
+            exp_date: 2031,
+        }
     },
     {
-        name: 'Tangir',
-        age: 15,
-        ismarried: true
+        model: 'cobalt',
+        company: 'general motors',
+        engine: 1.5,
+        price: 15000,
+        beatan: true,
+        year: 2022,
+        mileage: 13000,
+        tech_passport: {
+            date: 2022,
+            exp_date: 2032,
+        }
+
     },
     {
-        name: 'Sardor.X',
-        age: 17,
-        ismarried: true
+        model: 'Damas',
+        company: 'Chevrolet',
+        color: 'White',
+        engine: 2.4,
+        price: 10000,
+        beaten: false,
+        year: 2023,
+        mileage: 100,
+        tech_pasport: {
+            date: 2023,
+            exp_date: 2033
+        }
     },
     {
-        name: 'Ruslan',
-        age: 16,
-        ismarried: false
+        model: "Spark",
+        company: "general motors",
+        color: "dirtygreen",
+        engine: 1.250,
+        price: 7000,
+        beaten: true,
+        year: 2008,
+        mileage: 328000,
+        tech_passport: {
+            date: 2008,
+            exp_date: 2023,
+        }
     },
     {
-        name: 'Amira',
-        age: 16,
-        ismarried: false
+        model: "bmw m5",
+        engine: "бензиновыми двигателями рабочим объёмом 3453-4999 см3",
+        year_of_issue: 1998,
+        beaten: false,
+        price: 37000,
+        color: "black",
+        company: "BMW",
+        tech_passport: {
+            date: 2022,
+            exp_date: 2032,
+        }
+
     },
     {
-        name: 'Dilruh',
-        age: 16,
-        ismarried: false
+        model: "Labo",
+        company: "GM ",
+        color: "white",
+        engine: 0.8,
+        price: 10000,
+        beaten: false,
+        year: 2023,
+        mileage: 0,
+        tech_passport: {
+            date: 2023,
+            exp_date: 2033,
+        }
     },
     {
-        name: 'Samir',
-        age: 21,
-        ismarried: true
+        model: "tico",
+        company: "daewoo",
+        color: "black",
+        engine: 1.5,
+        price: 10000,
+        beaten: true,
+        year: 2010,
+        mileage: 1000,
+
+tech_passport: {
+            date: 2022,
+            exp_date: 2025,
+        }
     },
     {
-        name: 'MuhamadDamir',
-        age: 19,
-        ismarried: true
+        model: 'traverse',
+        company: 'chevrolet',
+        color: 'black',
+        engine: 3.6,
+        price: 60000,
+        beaten: false,
+        year: 2023,
+        mileage: 500,
+        tech_passport: {
+            date: 2023,
+            exp_date: 2033
+        }
+    },
+    {
+        model: "volga",
+        company: "uaz",
+        color: "white",
+        engine: 0.4,
+        price: 700000,
+        beaten: true,
+        year: 1964,
+        mileage: 3000000,
+        tech_passport: {
+            date: 1964,
+            exp_date: 2023,
+        }
+    },
+    {
+        model: "ВАЗ 2106",
+        company: "ВАЗ",
+        color: "dry asphalt",
+        engine: 1.5,
+        price: 3000,
+        beaten: true,
+        year: 1995,
+        mileage: 100000,
+        tech_passport: {
+            date: 1996,
+            exp_date: 2026,
+        }
+    },
+    {
+        model: "velik",
+        company: "Philips",
+        color: "red",
+        engine: "v12",
+        price: 12000,
+        beaten: false,
+        year: 2009,
+        mileage: 0,
+        tech_passport: {
+            date: 2022,
+            exp_date: 2032,
+        }
+    },
+    {
+        model: 'Tahoe',
+        company: 'CHEVROLET',
+        color: 'black',
+        engine: 5.3,
+        price: 110000,
+        beaten: false,
+        year: 2022,
+        mileage: 0,
+        tech_passport: {
+            date: 2022,
+            expDate: 2032
+        }
+    },
+    {
+        model: "Mercedes-Benz CLS",
+        company: "Mercedes-Benz",
+        color: "black",
+        engine: 3.0,
+        price: 110000,
+        beaten: false,
+        year: 2023,
+        mileage: 200,
+        tech_passport: {
+            date: 2023,
+            exp_date: 2033,
+        }
+    },
+    {
+        model: "malibu",
+        company: "Chevrolet",
+        color: "sky rim",
+        engine: 2.4,
+        price: 15000,
+        year: 2022,
+        clash: false,
+        mileage: 1500,
+        tech_passport: {
+            date: 2022,
+            exp_date: 2054
+        }
+    },
+    {
+        model: "matiz",
+        company: "general motors",
+        color: "yellow",
+        engine: 0.8,
+        price: 4000,
+        beaten: false,
+        year: 2018,
+        mileage: 5000,
+        tech_passport: {
+            date: 2018,
+            exp_date: 2028,
+        }
+    },
+    {
+        model: "porter",
+        company: "hyundai",
+        color: "blue",
+        engine: 2.5,
+        price: 31000,
+        beaten: false,
+        year: 2023,
+        mileage: 100,
+        tech_passport: {
+            date: 2023,
+            exp_date: 2033,
+        }
+    },
+    {
+        year: 2023,
+        price: 85000,
+        engine: 6.5,
+        Mileage: 11.29,
+        beaten: false,
+        color: "black"
     }
 ]
-let married = []
-let alone = []
-console.log(
-    arr.forEach((item) => {
-        if (item.ismarried === true){
-            married.push(arr.ismarried)
-        } else if(item === false) {
-            alone.push(arr.ismarried)
-        }
-    })
-)
 
 
+let from = prompt('from')
+let up = prompt('up')
 
-let average_age = 0
-arr.forEach(person => {
-    average_age += person.age
+
+let result = []
+
+cars.forEach(car => {
+    if(car.price >= from && car.price <= up) {
+        result.push(`model: ${car.model}, price: ${car.price} \n`)
+    }
 })
-average_age = average_age / arr.length
 
-console.log(average_age)
+
+let oneOfThem = prompt('По вашей ценовой категории найдено вот эти машины ' + result + " Выберите один из этого списка написав название машины" )
+let findedCar = cars.find((item) => item === oneOfThem)
+let answer = confirm("Do you want to buy")
+if(answer === true){
+    let money = prompt('Please pay')
+    if(money <= item.price ){
+        alert('You need more money.' )
+    }else if (money >= item.price){
+        let your_debt = []
+        your_debt = money - item.price  
+        alert('Okay,thanks')
+    }else if(money === item.price){
+        alert('Everything is ok')
+    }
+}
